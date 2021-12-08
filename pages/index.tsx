e78e909366e2b0d250f44852bc1a4ed430d79922
倪永胜 <714275326@qq.com>
2021-12-08 10:45:25 +0800

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,10 @@
+import { Box, Flex } from "@chakra-ui/layout";
+import React from "react";
 
-export default function HomePage () {
+export default function HomePage() {
   return (
-    <>
-    <div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div>
-    <div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div>
-    <div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div>
-    <div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div>
-    <div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div>
-    <div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div>
-    <div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div>
-    <div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div>
-    <div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div>
-    <div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div>
-    <div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div><div>home</div></>
+    <Flex justifyContent="center" alignItems="center" height="calc(100vh - 100px)">
+      <Box fontSize='20px'>Click left menu to submit your proposal.</Box>
+    </Flex>
   );
 }
