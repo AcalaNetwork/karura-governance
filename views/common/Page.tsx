6430e9d69a4e9dd18ae4d90cfc1f40b03890ab65
倪永胜 <714275326@qq.com>
2021-12-10 18:46:35 +0800

    fix-ui

--- views/common/Page.tsx
+++ views/common/Page.tsx
@@ -5,14 +5,14 @@ import { SideBar } from "./SideBar";
 
 export const Page: FC = ({ children }) => {
   return (
-    <Box width="100%" height="100vh" overflowX='hidden' overflowY='auto'>
-          <Header></Header>
-          <Flex height="calc(100% - 60px)">
-            <SideBar />
-            <Box flex="1" maxHeight="calc(100vh - 60px)" overflow="scroll" padding="20px" boxSizing="border-box">
-              {children}
-            </Box>
-          </Flex>
+    <Box width="100%" height="100vh" overflow="hidden">
+      <Header></Header>
+      <Flex height="calc(100% - 60px)">
+        <SideBar />
+        <Box flex="1" maxHeight="calc(100vh - 60px)" overflowY='auto' overflowX='hidden' padding="20px" boxSizing="border-box">
+          {children}
         </Box>
+      </Flex>
+    </Box>
   );
 };
